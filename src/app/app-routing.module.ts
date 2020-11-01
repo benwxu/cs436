e4d024c975ef2efb122ab4865408d0e17713f0c7
WeiJian Xu <wx002@bucknell.edu>
2020-11-01 16:54:10 -0600

    finish hw3 with routing and added about component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { AboutComponent } from './about/about.component';
+import { FlightCostComponent } from './FlightCost/FlightCost.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'flightcost', component: FlightCostComponent},
+  {path: 'about', component: AboutComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import {FlightCostComponent} from './FlightCost/FlightCost.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlightCostComponent
+    FlightCostComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent, FlightCostComponent]
